2dcb4c2ffa6720cc4061ea0be00f7ed64c114c44
Christian Ihle <blurpy@gmail.com>
2011-11-20 15:55:22 +0100

    Javadoc for controller and service packages

---
+++ src/main/java/net/usikkert/kouchat/android/controller/package-info.java
@@ -0,0 +1,27 @@
+
+/***************************************************************************
+ *   Copyright 2006-2011 by Christian Ihle                                 *
+ *   kontakt@usikkert.net                                                  *
+ *                                                                         *
+ *   This file is part of KouChat.                                         *
+ *                                                                         *
+ *   KouChat is free software; you can redistribute it and/or modify       *
+ *   it under the terms of the GNU General Public License as               *
+ *   published by the Free Software Foundation, either version 3 of        *
+ *   the License, or (at your option) any later version.                   *
+ *                                                                         *
+ *   KouChat is distributed in the hope that it will be useful,            *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the          *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with KouChat. If not, see <http://www.gnu.org/licenses/>.       *
+ ***************************************************************************/
+
+/**
+ * This package contains Android controllers.
+ *
+ * @author Christian Ihle
+ */
+package net.usikkert.kouchat.android.controller;
