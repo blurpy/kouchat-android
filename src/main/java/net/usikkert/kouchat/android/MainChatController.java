c72ac8dfb0aad7a42595e0311a648282364e53b5
Christian Ihle <blurpy@gmail.com>
2011-11-20 13:40:02 +0100

    Adding some android resources

---
+++ src/main/java/net/usikkert/kouchat/android/MainChatController.java
@@ -0,0 +1,10 @@
+package net.usikkert.kouchat.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainChatController extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
