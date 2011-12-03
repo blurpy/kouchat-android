4c4782d5c746e25f3f4b2d06e9a19df10ff2bca7
Christian Ihle <blurpy@gmail.com>
2011-12-03 23:07:32 +0100

    Making the test more realistic - but it fails

--- kouchat-android-test/src/main/java/net/usikkert/kouchat/android/SettingsTest.java
+++ kouchat-android-test/src/main/java/net/usikkert/kouchat/android/SettingsTest.java
@@ -21,37 +21,38 @@
 
 package net.usikkert.kouchat.android;
 
-import net.usikkert.kouchat.android.controller.SettingsController;
+import net.usikkert.kouchat.android.controller.MainChatController;
 
 import com.jayway.android.robotium.solo.Solo;
 
 import android.test.ActivityInstrumentationTestCase2;
 
 /**
- * Test of {@link SettingsController}.
+ * Test of changing the settings.
  *
  * @author Christian Ihle
  */
 
-public class SettingsControllerTest extends ActivityInstrumentationTestCase2<SettingsController> {
+public class SettingsTest extends ActivityInstrumentationTestCase2<MainChatController> {
 
     private Solo solo;
 
-    public SettingsControllerTest() {
-        super(SettingsController.class);
-    }
-
-    public void testActivity() {
-        final SettingsController activity = getActivity();
-        assertNotNull(activity);
+    public SettingsTest() {
+        super(MainChatController.class);
     }
 
     public void setUp() {
         solo = new Solo(getInstrumentation(), getActivity());
     }
 
-    public void testPreferenceIsSaved() {
+    public void testActivity() {
+        final MainChatController activity = getActivity();
+        assertNotNull(activity);
+    }
+
+    public void testNickNameIsSaved() {
         solo.sendKey(Solo.MENU);
+        solo.clickOnText("Settings");
         solo.clickOnText("Set nick name");
         assertTrue(solo.searchText("Set nick name"));
 
@@ -59,6 +60,18 @@ public class SettingsControllerTest extends ActivityInstrumentationTestCase2<Set
         solo.enterText(0, "ape");
         solo.clickOnButton("OK");
         assertTrue(solo.searchText("ape"));
+
+        solo.goBack();
+        assertTrue(solo.searchText("You changed nick to ape")); // This fails for some reason...
+    }
+
+    public void testResetNickName() {
+        solo.sendKey(Solo.MENU);
+        solo.clickOnText("Settings");
+        solo.clickOnText("Set nick name");
+        solo.clearEditText(0);
+        solo.enterText(0, "test");
+        solo.clickOnButton("OK");
     }
 
     public void tearDown() {
