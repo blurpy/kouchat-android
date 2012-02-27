b4ce7370894acb1cad5b939727818f767141f116
Christian Ihle <blurpy@gmail.com>
2012-02-27 18:31:01 +0100

    Adding topic tests

--- kouchat-android-test/src/main/java/net/usikkert/kouchat/android/TopicTest.java
+++ kouchat-android-test/src/main/java/net/usikkert/kouchat/android/TopicTest.java
@@ -22,6 +22,12 @@
 package net.usikkert.kouchat.android;
 
 import net.usikkert.kouchat.android.controller.MainChatController;
+import net.usikkert.kouchat.misc.CommandException;
+import net.usikkert.kouchat.misc.Settings;
+import net.usikkert.kouchat.misc.Topic;
+import net.usikkert.kouchat.misc.User;
+import net.usikkert.kouchat.net.Messages;
+import net.usikkert.kouchat.util.TestClient;
 
 import com.jayway.android.robotium.solo.Solo;
 
@@ -30,28 +36,58 @@ import android.test.ActivityInstrumentationTestCase2;
 /**
  * Test of topic handling.
  *
- * TODO
+ * <p>These tests must run in order to pass.</p>
  *
  * @author Christian Ihle
  */
-
 public class TopicTest extends ActivityInstrumentationTestCase2<MainChatController> {
 
     private Solo solo;
+    private TestClient client;
+    private Messages messages;
+    private MainChatController activity;
+    private User me;
 
     public TopicTest() {
         super(MainChatController.class);
     }
 
     public void setUp() {
-//        solo = new Solo(getInstrumentation(), getActivity());
+        activity = getActivity();
+        solo = new Solo(getInstrumentation(), activity);
+        client = new TestClient();
+        messages = client.logon();
+        me = Settings.getSettings().getMe();
     }
 
-    public void testFail() {
-        fail("Not implemented");
+    public void test1OtherClientSettingTopicIsShownInChatAndTitle() throws CommandException {
+        messages.sendTopicChangeMessage(new Topic("Original topic", "Test", System.currentTimeMillis()));
+        sleep(500);
+
+        assertTrue(solo.searchText("Topic is: Original topic"));
+        assertEquals(me.getNick() + " - Topic: Original topic (Test) - KouChat", activity.getTitle());
+    }
+
+    public void test2OtherClientChangingTopicIsShownInChatAndTitle() throws CommandException {
+        messages.sendTopicChangeMessage(new Topic("New topic", "Test", System.currentTimeMillis()));
+        sleep(500);
+
+        assertTrue(solo.searchText("Test changed the topic to: New topic"));
+        assertEquals(me.getNick() + " - Topic: New topic (Test) - KouChat", activity.getTitle());
     }
 
     public void tearDown() {
-//        solo.finishOpenedActivities();
+        client.logoff();
+        solo.finishOpenedActivities();
+    }
+
+    private void sleep(final int time) {
+        try {
+            Thread.sleep(time);
+        }
+
+        catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
