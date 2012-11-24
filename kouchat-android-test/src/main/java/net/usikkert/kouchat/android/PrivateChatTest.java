4ac311b15158f5a298cbbb9bb26d5aa9ea0118f5
Christian Ihle <blurpy@gmail.com>
2012-11-24 22:29:00 +0100

    Adding a couple of simple tests for the private chat. Writing messages are still failing

--- kouchat-android-test/src/main/java/net/usikkert/kouchat/android/PrivateChatTest.java
+++ kouchat-android-test/src/main/java/net/usikkert/kouchat/android/PrivateChatTest.java
@@ -23,6 +23,8 @@
 package net.usikkert.kouchat.android;
 
 import net.usikkert.kouchat.android.controller.MainChatController;
+import net.usikkert.kouchat.android.controller.PrivateChatController;
+import net.usikkert.kouchat.util.TestUtils;
 
 import com.jayway.android.robotium.solo.Solo;
 
@@ -44,14 +46,33 @@ public class PrivateChatTest extends ActivityInstrumentationTestCase2<MainChatCo
     }
 
     public void setUp() {
-//        solo = new Solo(getInstrumentation(), getActivity());
+        solo = new Solo(getInstrumentation(), getActivity());
     }
 
-    public void testFail() {
-        fail("Not implemented");
+    public void test01ClickingOnUserShouldOpenPrivateChat() {
+        openPrivateChat();
+        solo.assertCurrentActivity("Should have opened the private chat", PrivateChatController.class);
+    }
+
+    public void test02OwnMessageIsShownInChat() {
+        openPrivateChat();
+
+        TestUtils.writeLine(solo, "This is a new message from myself");
+        solo.sleep(500);
+
+        assertTrue(TestUtils.searchText(solo, "This is a new message from myself"));
+    }
+
+    public void test99Quit() {
+        TestUtils.quit(solo);
     }
 
     public void tearDown() {
-//        solo.finishOpenedActivities();
+        solo.finishOpenedActivities();
+    }
+
+    private void openPrivateChat() {
+        solo.clickInList(1);
+        solo.sleep(500);
     }
 }
