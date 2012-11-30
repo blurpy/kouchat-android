ba6ea36eb0035b6091b7bb8c15b6882df3c4b216
Christian Ihle <blurpy@gmail.com>
2012-11-30 19:00:51 +0100

    Adding user list test of sorting

--- kouchat-android-test/src/main/java/net/usikkert/kouchat/android/UserListTest.java
+++ kouchat-android-test/src/main/java/net/usikkert/kouchat/android/UserListTest.java
@@ -25,6 +25,7 @@ package net.usikkert.kouchat.android;
 import net.usikkert.kouchat.android.controller.MainChatController;
 import net.usikkert.kouchat.misc.Settings;
 import net.usikkert.kouchat.misc.User;
+import net.usikkert.kouchat.net.Messages;
 import net.usikkert.kouchat.util.TestClient;
 import net.usikkert.kouchat.util.TestUtils;
 
@@ -43,48 +44,62 @@ public class UserListTest extends ActivityInstrumentationTestCase2<MainChatContr
     private Solo solo;
     private TestClient client;
     private User me;
+    private ListView userList;
 
     public UserListTest() {
         super(MainChatController.class);
     }
 
     public void setUp() {
+        me = Settings.getSettings().getMe();
+        me.setNick("Kou");
+
         final MainChatController activity = getActivity();
         solo = new Solo(getInstrumentation(), activity);
         client = new TestClient();
-        me = Settings.getSettings().getMe();
+
+        solo.sleep(100);
+
+        userList = solo.getCurrentListViews().get(0);
     }
 
     public void test01UserListShouldContainMeOnLogon() {
-        final ListView userList = solo.getCurrentListViews().get(0);
         assertEquals(1, userList.getCount());
         assertSame(me, userList.getItemAtPosition(0));
     }
 
     public void test02UserListShouldAddNewUser() {
-        final ListView userList = solo.getCurrentListViews().get(0);
         assertEquals(1, userList.getCount());
 
         client.logon();
-
         solo.sleep(500);
 
         assertEquals(2, userList.getCount());
-
-        final User user1 = (User) userList.getItemAtPosition(0);
-        final User user2 = (User) userList.getItemAtPosition(1);
-
-        assertSame(me, user1);
-        assertEquals("Test", user2.getNick());
     }
 
-    public void test03OrientationSwitchShouldKeepUserList() {
-        assertEquals(me, solo.getCurrentListViews().get(0).getItemAtPosition(0));
+    public void test03UserListShouldBeSorted() {
+        final Messages messages = client.logon();
+        solo.sleep(500);
+
+        assertEquals("Kou", getUserNameAtPosition(0));
+        assertEquals("Test", getUserNameAtPosition(1));
+
+        messages.sendNickMessage("Ape");
+        solo.sleep(500);
+
+        assertEquals("Ape", getUserNameAtPosition(0));
+        assertEquals("Kou", getUserNameAtPosition(1));
+    }
+
+    public void test04OrientationSwitchShouldKeepSortedUserList() {
+        client.logon();
+        solo.sleep(500);
 
         solo.setActivityOrientation(Solo.PORTRAIT);
         solo.sleep(500);
 
-        assertEquals(me, solo.getCurrentListViews().get(0).getItemAtPosition(0));
+        assertEquals("Kou", getUserNameAtPosition(0));
+        assertEquals("Test", getUserNameAtPosition(1));
     }
 
     public void test99Quit() {
@@ -95,5 +110,13 @@ public class UserListTest extends ActivityInstrumentationTestCase2<MainChatContr
     public void tearDown() {
         client.logoff();
         solo.finishOpenedActivities();
+    }
+
+    private String getUserNameAtPosition(final int position) {
+        return getUserAtPosition(position).getNick();
+    }
+
+    private User getUserAtPosition(final int position) {
+        return (User) userList.getItemAtPosition(position);
     }
 }
