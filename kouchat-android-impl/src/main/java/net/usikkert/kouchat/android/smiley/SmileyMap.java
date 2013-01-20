477650ae9bc3991acea346e5c2329365b7643c57
Christian Ihle <blurpy@gmail.com>
2013-01-20 13:54:18 +0100

    Adding map with the supported smileys

---
+++ kouchat-android-impl/src/main/java/net/usikkert/kouchat/android/smiley/SmileyMap.java
@@ -0,0 +1,122 @@
+
+/***************************************************************************
+ *   Copyright 2006-2012 by Christian Ihle                                 *
+ *   kontakt@usikkert.net                                                  *
+ *                                                                         *
+ *   This file is part of KouChat.                                         *
+ *                                                                         *
+ *   KouChat is free software; you can redistribute it and/or modify       *
+ *   it under the terms of the GNU Lesser General Public License as        *
+ *   published by the Free Software Foundation, either version 3 of        *
+ *   the License, or (at your option) any later version.                   *
+ *                                                                         *
+ *   KouChat is distributed in the hope that it will be useful,            *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      *
+ *   Lesser General Public License for more details.                       *
+ *                                                                         *
+ *   You should have received a copy of the GNU Lesser General Public      *
+ *   License along with KouChat.                                           *
+ *   If not, see <http://www.gnu.org/licenses/>.                           *
+ ***************************************************************************/
+
+package net.usikkert.kouchat.android.smiley;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+import net.usikkert.kouchat.android.R;
+import net.usikkert.kouchat.util.Validate;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
+
+/**
+ * This class represents a map of all the supported smileys, with the smiley code as the key,
+ * and the drawable as the value.
+ *
+ * <p>Supported smiley codes:</p>
+ *
+ * <ul>
+ *   <li>:)</li>
+ *   <li>:(</li>
+ *   <li>:p</li>
+ *   <li>:D</li>
+ *   <li>;)</li>
+ *   <li>:O</li>
+ *   <li>:@</li>
+ *   <li>:S</li>
+ *   <li>;(</li>
+ *   <li>:$</li>
+ *   <li>8)</li>
+ * </ul>
+ *
+ * @author Christian Ihle
+ */
+public class SmileyMap {
+
+    private final Map<String, Drawable> smileyMap;
+
+    /**
+     * Constructor. Loads all the smileys in the map.
+     *
+     * @param context An Android context, for loading resources.
+     */
+    public SmileyMap(final Context context) {
+        Validate.notNull(context, "Context can not be null");
+
+        smileyMap = new HashMap<String, Drawable>();
+
+        loadSmileys(context.getResources());
+    }
+
+    /**
+     * Gets the smiley with the specified code. Like "<code>:)</code>"
+     *
+     * @param code The code of the smiley to get.
+     * @return The smiley with the specified code.
+     */
+    public Drawable getSmiley(final String code) {
+        Validate.notEmpty(code, "Smiley code can not be empty");
+
+        if (!smileyMap.containsKey(code)) {
+            throw new IllegalArgumentException(String.format("Smiley with code '%s' does not exist", code));
+        }
+
+        return smileyMap.get(code);
+    }
+
+    /**
+     * Gets a set of all the smiley codes in this map.
+     *
+     * @return A set of all the smiley codes.
+     */
+    public Set<String> getSmileyCodes() {
+        return smileyMap.keySet();
+    }
+
+    private void loadSmileys(final Resources resources) {
+        smileyMap.put(":)", getDrawable(resources, R.drawable.smiley_smile));
+        smileyMap.put(":(", getDrawable(resources, R.drawable.smiley_sad));
+        smileyMap.put(":p", getDrawable(resources, R.drawable.smiley_tongue));
+        smileyMap.put(":D", getDrawable(resources, R.drawable.smiley_teeth));
+        smileyMap.put(";)", getDrawable(resources, R.drawable.smiley_wink));
+        smileyMap.put(":O", getDrawable(resources, R.drawable.smiley_omg));
+        smileyMap.put(":@", getDrawable(resources, R.drawable.smiley_angry));
+        smileyMap.put(":S", getDrawable(resources, R.drawable.smiley_confused));
+        smileyMap.put(";(", getDrawable(resources, R.drawable.smiley_cry));
+        smileyMap.put(":$", getDrawable(resources, R.drawable.smiley_embarrassed));
+        smileyMap.put("8)", getDrawable(resources, R.drawable.smiley_shade));
+    }
+
+    private Drawable getDrawable(final Resources resources, final int smileyId) {
+        final Drawable drawable = resources.getDrawable(smileyId);
+
+        // For some reason, the bounds is set to 0, with the result that the smiley becomes invisible. Fixing manually.
+        drawable.setBounds(0, 0, drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight());
+
+        return drawable;
+    }
+}
---
+++ kouchat-android-impl/src/test/java/net/usikkert/kouchat/android/smiley/SmileyMapTest.java
@@ -0,0 +1,146 @@
+
+/***************************************************************************
+ *   Copyright 2006-2012 by Christian Ihle                                 *
+ *   kontakt@usikkert.net                                                  *
+ *                                                                         *
+ *   This file is part of KouChat.                                         *
+ *                                                                         *
+ *   KouChat is free software; you can redistribute it and/or modify       *
+ *   it under the terms of the GNU Lesser General Public License as        *
+ *   published by the Free Software Foundation, either version 3 of        *
+ *   the License, or (at your option) any later version.                   *
+ *                                                                         *
+ *   KouChat is distributed in the hope that it will be useful,            *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      *
+ *   Lesser General Public License for more details.                       *
+ *                                                                         *
+ *   You should have received a copy of the GNU Lesser General Public      *
+ *   License along with KouChat.                                           *
+ *   If not, see <http://www.gnu.org/licenses/>.                           *
+ ***************************************************************************/
+
+package net.usikkert.kouchat.android.smiley;
+
+import static org.junit.Assert.*;
+
+import java.util.Set;
+
+import net.usikkert.kouchat.android.R;
+import net.usikkert.kouchat.android.controller.MainChatController;
+
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+
+import com.xtremelabs.robolectric.Robolectric;
+import com.xtremelabs.robolectric.RobolectricTestRunner;
+
+import android.graphics.Rect;
+import android.graphics.drawable.Drawable;
+
+/**
+ * Test of {@link SmileyMap}.
+ *
+ * @author Christian Ihle
+ */
+@RunWith(RobolectricTestRunner.class)
+public class SmileyMapTest {
+
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
+
+    private SmileyMap smileyMap;
+
+    @Before
+    public void setUp() {
+        smileyMap = new SmileyMap(new MainChatController());
+    }
+
+    @Test
+    public void constructorShouldThrowExceptionIfContextIsNull() {
+        expectedException.expect(IllegalArgumentException.class);
+        expectedException.expectMessage("Context can not be null");
+
+        new SmileyMap(null);
+    }
+
+    @Test
+    public void getSmileyCodesShouldContainAllElevenSmileys() {
+        final Set<String> textSmileys = smileyMap.getSmileyCodes();
+
+        assertNotNull(textSmileys);
+        assertEquals(11, textSmileys.size());
+
+        assertTrue(textSmileys.contains(":)"));
+        assertTrue(textSmileys.contains(":("));
+        assertTrue(textSmileys.contains(":p"));
+        assertTrue(textSmileys.contains(":D"));
+
+        assertTrue(textSmileys.contains(";)"));
+        assertTrue(textSmileys.contains(":O"));
+        assertTrue(textSmileys.contains(":@"));
+        assertTrue(textSmileys.contains(":S"));
+
+        assertTrue(textSmileys.contains(";("));
+        assertTrue(textSmileys.contains(":$"));
+        assertTrue(textSmileys.contains("8)"));
+    }
+
+    @Test
+    public void getSmileyShouldThrowExceptionIfCodeIsNull() {
+        expectedException.expect(IllegalArgumentException.class);
+        expectedException.expectMessage("Smiley code can not be empty");
+
+        smileyMap.getSmiley(null);
+    }
+
+    @Test
+    public void getSmileyShouldThrowExceptionIfCodeIsEmpty() {
+        expectedException.expect(IllegalArgumentException.class);
+        expectedException.expectMessage("Smiley code can not be empty");
+
+        smileyMap.getSmiley(" ");
+    }
+
+    @Test
+    public void getSmileyShouldThrowExceptionIfCodeIsNotFound() {
+        expectedException.expect(IllegalArgumentException.class);
+        expectedException.expectMessage("Smiley with code ':%' does not exist");
+
+        smileyMap.getSmiley(":%");
+    }
+
+    @Test
+    public void getSmileyShouldSupportAllElevenSmileys() {
+        assertEquals(R.drawable.smiley_smile, smileyId(":)"));
+        assertEquals(R.drawable.smiley_sad, smileyId(":("));
+        assertEquals(R.drawable.smiley_tongue, smileyId(":p"));
+        assertEquals(R.drawable.smiley_teeth, smileyId(":D"));
+
+        assertEquals(R.drawable.smiley_wink, smileyId(";)"));
+        assertEquals(R.drawable.smiley_omg, smileyId(":O"));
+        assertEquals(R.drawable.smiley_angry, smileyId(":@"));
+        assertEquals(R.drawable.smiley_confused, smileyId(":S"));
+
+        assertEquals(R.drawable.smiley_cry, smileyId(";("));
+        assertEquals(R.drawable.smiley_embarrassed, smileyId(":$"));
+        assertEquals(R.drawable.smiley_shade, smileyId("8)"));
+    }
+
+    @Test
+    @Ignore("This does not work with Robolectric yet.")
+    public void getSmileyShouldReturnDrawableWithCorrectBounds() {
+        final Drawable smiley = smileyMap.getSmiley(":)");
+
+        assertEquals(new Rect(0, 0, 22, 22), smiley.getBounds());
+
+    }
+
+    private int smileyId(final String code) {
+        return Robolectric.shadowOf(smileyMap.getSmiley(code)).getLoadedFromResourceId();
+    }
+}
