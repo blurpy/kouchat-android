3bb82664f5c79f4e4ae0417fc102e12374f8bc10
Christian Ihle <blurpy@gmail.com>
2013-04-07 15:04:06 +0200

    Preparing for development of v0.9.0

--- kouchat-android-impl/src/main/java/net/usikkert/kouchat/Constants.java
+++ kouchat-android-impl/src/main/java/net/usikkert/kouchat/Constants.java
@@ -37,7 +37,7 @@ public interface Constants {
     /**
      * The application version.
      */
-    String APP_VERSION = "0.8.0";
+    String APP_VERSION = "0.9.0";
 
     /**
      * Which license the application has.
