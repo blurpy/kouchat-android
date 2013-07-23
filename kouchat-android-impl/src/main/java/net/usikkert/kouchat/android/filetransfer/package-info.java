820f9aa305d1bfc204cf28338bb76edeabab875a
Christian Ihle <blurpy@gmail.com>
2013-07-23 18:20:13 +0200

    Adding missing package-infos

---
+++ kouchat-android-impl/src/main/java/net/usikkert/kouchat/android/filetransfer/package-info.java
@@ -0,0 +1,28 @@
+
+/***************************************************************************
+ *   Copyright 2006-2013 by Christian Ihle                                 *
+ *   kontakt@usikkert.net                                                  *
+ *                                                                         *
+ *   This file is part of KouChat.                                         *
+ *                                                                         *
+ *   KouChat is free software; you can redistribute it and/or modify       *
+ *   it under the terms of the GNU Lesser General Public License as        *
+ *   published by the Free Software Foundation, either version 3 of        *
+ *   the License, or (at your option) any later version.                   *
+ *                                                                         *
+ *   KouChat is distributed in the hope that it will be useful,            *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      *
+ *   Lesser General Public License for more details.                       *
+ *                                                                         *
+ *   You should have received a copy of the GNU Lesser General Public      *
+ *   License along with KouChat.                                           *
+ *   If not, see <http://www.gnu.org/licenses/>.                           *
+ ***************************************************************************/
+
+/**
+ * This package contains classes related to file transfer.
+ *
+ * @author Christian Ihle
+ */
+package net.usikkert.kouchat.android.filetransfer;
