a5914065e3342f9c2a55990c12e40c4735e5e5bb
Christian Ihle <blurpy@gmail.com>
2014-02-18 17:44:02 +0100

    Forgot to null instrumentation

--- kouchat-android-test/src/main/java/net/usikkert/kouchat/android/TopicTest.java
+++ kouchat-android-test/src/main/java/net/usikkert/kouchat/android/TopicTest.java
@@ -178,6 +178,7 @@ public class TopicTest extends ActivityInstrumentationTestCase2<MainChatControll
         solo.finishOpenedActivities();
 
         solo = null;
+        instrumentation = null;
         me = null;
         setActivity(null);
 
