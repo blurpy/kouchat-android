7b5cf2aeaf442dab1f988a091ab05ff1ffcf4584
Christian Ihle <blurpy@gmail.com>
2014-05-31 11:54:06 +0200

    Reverting a change that was not supposed to be copied from kouchat 1.3.0

--- kouchat-android-impl/src/test/java/net/usikkert/kouchat/util/ToolsTest.java
+++ kouchat-android-impl/src/test/java/net/usikkert/kouchat/util/ToolsTest.java
@@ -255,7 +255,18 @@ public class ToolsTest {
         assertEquals("this.is.a.song_2.ogg", file.getName());
     }
 
-    private File createTemporaryFile(final String fileName) throws IOException {
+    /**
+     * Creates a file that will be deleted when the jvm exists.
+     *
+     * @param file The file to create.
+     * @return The created file.
+     * @throws IOException If something goes wrong.
+     */
+    public static File createTemporaryFile(final File file) throws IOException {
+        return createTemporaryFile(file.getAbsolutePath());
+    }
+
+    private static File createTemporaryFile(final String fileName) throws IOException {
         final File file = new File(fileName);
 
         if (!file.exists()) {
