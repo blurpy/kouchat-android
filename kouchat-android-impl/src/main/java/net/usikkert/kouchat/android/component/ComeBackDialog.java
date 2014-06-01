7fc70592e497671501a35cb8021d95e9d2fd834f
Christian Ihle <blurpy@gmail.com>
2014-06-01 17:07:50 +0200

    Adding dialog for coming back from away

---
+++ kouchat-android-impl/src/main/java/net/usikkert/kouchat/android/component/ComeBackDialog.java
@@ -0,0 +1,86 @@
+
+/***************************************************************************
+ *   Copyright 2006-2014 by Christian Ihle                                 *
+ *   contact@kouchat.net                                                   *
+ *                                                                         *
+ *   This file is part of KouChat.                                         *
+ *                                                                         *
+ *   KouChat is free software; you can redistribute it and/or modify       *
+ *   it under the terms of the GNU Lesser General Public License as        *
+ *   published by the Free Software Foundation, either version 3 of        *
+ *   the License, or (at your option) any later version.                   *
+ *                                                                         *
+ *   KouChat is distributed in the hope that it will be useful,            *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      *
+ *   Lesser General Public License for more details.                       *
+ *                                                                         *
+ *   You should have received a copy of the GNU Lesser General Public      *
+ *   License along with KouChat.                                           *
+ *   If not, see <http://www.gnu.org/licenses/>.                           *
+ ***************************************************************************/
+
+package net.usikkert.kouchat.android.component;
+
+import net.usikkert.kouchat.android.R;
+import net.usikkert.kouchat.android.chatwindow.AndroidUserInterface;
+import net.usikkert.kouchat.util.Validate;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.view.ContextThemeWrapper;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+
+/**
+ * Creates a dialog for coming back from away.
+ *
+ * @author Christian Ihle
+ */
+public class ComeBackDialog {
+
+    public ComeBackDialog(final Context context, final AndroidUserInterface androidUserInterface) {
+        Validate.notNull(context, "Context can not be null");
+        Validate.notNull(androidUserInterface, "AndroidUserInterface can not be null");
+
+        final Context wrappedContext = new ContextThemeWrapper(context, R.style.Theme_Default_Dialog);
+        final LayoutInflater inflater = LayoutInflater.from(wrappedContext);
+
+        final View comeBackDialog = inflater.inflate(R.layout.come_back_dialog, null);
+        final TextView comeBackDialogMessage = (TextView) comeBackDialog.findViewById(R.id.comeBackDialogMessage);
+
+        setComeBackMessage(androidUserInterface, wrappedContext, comeBackDialogMessage);
+
+        final AlertDialog alertDialog = createComeBackDialog(androidUserInterface, wrappedContext, comeBackDialog);
+
+        alertDialog.show();
+    }
+
+    private void setComeBackMessage(final AndroidUserInterface ui, final Context context, final TextView comeBackDialogMessage) {
+        final String awayMessage = ui.getMe().getAwayMsg();
+        final String comeBackMessage = context.getString(R.string.come_back_dialog_message, awayMessage);
+
+        comeBackDialogMessage.setText(comeBackMessage);
+    }
+
+    private AlertDialog createComeBackDialog(final AndroidUserInterface ui, final Context context, final View comeBackDialog) {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(context);
+
+        builder.setView(comeBackDialog);
+        builder.setTitle(R.string.away);
+        builder.setIcon(R.drawable.ic_dialog);
+
+        builder.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(final DialogInterface dialog, final int which) {
+                ui.comeBack();
+            }
+        });
+
+        builder.setNegativeButton(android.R.string.cancel, null);
+
+        return builder.create();
+    }
+}
---
+++ kouchat-android-impl/src/test/java/net/usikkert/kouchat/android/component/ComeBackDialogTest.java
@@ -0,0 +1,146 @@
+
+/***************************************************************************
+ *   Copyright 2006-2014 by Christian Ihle                                 *
+ *   contact@kouchat.net                                                   *
+ *                                                                         *
+ *   This file is part of KouChat.                                         *
+ *                                                                         *
+ *   KouChat is free software; you can redistribute it and/or modify       *
+ *   it under the terms of the GNU Lesser General Public License as        *
+ *   published by the Free Software Foundation, either version 3 of        *
+ *   the License, or (at your option) any later version.                   *
+ *                                                                         *
+ *   KouChat is distributed in the hope that it will be useful,            *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      *
+ *   Lesser General Public License for more details.                       *
+ *                                                                         *
+ *   You should have received a copy of the GNU Lesser General Public      *
+ *   License along with KouChat.                                           *
+ *   If not, see <http://www.gnu.org/licenses/>.                           *
+ ***************************************************************************/
+
+package net.usikkert.kouchat.android.component;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+import net.usikkert.kouchat.android.R;
+import net.usikkert.kouchat.android.chatwindow.AndroidUserInterface;
+import net.usikkert.kouchat.misc.User;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.shadows.ShadowAlertDialog;
+import org.robolectric.shadows.ShadowContextThemeWrapper;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.view.ContextThemeWrapper;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+/**
+ * Test of {@link ComeBackDialog}.
+ *
+ * @author Christian Ihle
+ */
+@RunWith(RobolectricTestRunner.class)
+public class ComeBackDialogTest {
+
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
+
+    private ShadowAlertDialog shadowDialog;
+    private AlertDialog dialog;
+
+    private TextView dialogMessage;
+
+    private AndroidUserInterface ui;
+
+    @Before
+    public void setUp() {
+        ui = mock(AndroidUserInterface.class);
+
+        final User user = new User("Test", 1234);
+        user.setAwayMsg("I went to the pub");
+        when(ui.getMe()).thenReturn(user);
+
+        new ComeBackDialog(Robolectric.application, ui); // Dialog would be shown after this
+
+        dialog = ShadowAlertDialog.getLatestAlertDialog();
+        shadowDialog = Robolectric.shadowOf(dialog);
+
+        final View dialogView = shadowDialog.getView();
+        dialogMessage = (TextView) dialogView.findViewById(R.id.comeBackDialogMessage);
+    }
+
+    @Test
+    public void constructorShouldThrowExceptionIfContextIsNull() {
+        expectedException.expect(IllegalArgumentException.class);
+        expectedException.expectMessage("Context can not be null");
+
+        new ComeBackDialog(null, ui);
+    }
+
+    @Test
+    public void constructorShouldThrowExceptionIfAndroidUserInterfaceIsNull() {
+        expectedException.expect(IllegalArgumentException.class);
+        expectedException.expectMessage("AndroidUserInterface can not be null");
+
+        new ComeBackDialog(Robolectric.application, null);
+    }
+
+    @Test
+    public void dialogTitleShouldBeSet() {
+        assertEquals("Away", shadowDialog.getTitle());
+    }
+
+    @Test
+    public void dialogIconShouldBeSet() {
+        assertEquals(R.drawable.ic_dialog, shadowDialog.getShadowAlertController().getIconId());
+    }
+
+    @Test
+    public void dialogThemeShouldBeSet() {
+        final ContextThemeWrapper context = (ContextThemeWrapper) dialog.getContext();
+        final ContextThemeWrapper baseContext = (ContextThemeWrapper) context.getBaseContext();
+        final ShadowContextThemeWrapper shadowBaseContext = (ShadowContextThemeWrapper) Robolectric.shadowOf(baseContext);
+        final int themeResId = shadowBaseContext.callGetThemeResId();
+
+        assertEquals(R.style.Theme_Default_Dialog, themeResId);
+    }
+
+    @Test
+    public void dialogMessageShouldBeSetUsingTheAwayMessage() {
+        assertEquals("Come back from 'I went to the pub'?", dialogMessage.getText());
+    }
+
+    @Test
+    public void dialogShouldHaveCancelButtonThatDoesNothingButClose() {
+        final Button button = dialog.getButton(DialogInterface.BUTTON_NEGATIVE);
+        assertEquals("Cancel", button.getText());
+
+        button.performClick();
+
+        verify(ui, never()).comeBack();
+        assertFalse(dialog.isShowing());
+    }
+
+    @Test
+    public void dialogShouldHaveOKButtonThatSetsBackAndCloses() {
+        final Button button = dialog.getButton(DialogInterface.BUTTON_POSITIVE);
+        assertEquals("OK", button.getText());
+
+        button.performClick();
+
+        verify(ui).comeBack();
+        assertFalse(dialog.isShowing());
+    }
+}
