1e19173c30149a7cb6ebbdae87c2b426dd7bf037
Christian Ihle <blurpy@gmail.com>
2014-06-10 19:03:45 +0200

    Changing to version 1.0.0

--- kouchat-android-impl/src/main/java/net/usikkert/kouchat/Constants.java
+++ kouchat-android-impl/src/main/java/net/usikkert/kouchat/Constants.java
@@ -37,7 +37,7 @@ public interface Constants {
     /**
      * The application version.
      */
-    String APP_VERSION = "0.15.0";
+    String APP_VERSION = "1.0.0";
 
     /**
      * Which license the application has.
