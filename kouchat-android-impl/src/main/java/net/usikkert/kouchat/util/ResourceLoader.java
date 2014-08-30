1785b87cd99102d50f236e174daff8055cc084ad
Christian Ihle <blurpy@gmail.com>
2014-08-30 18:18:27 +0200

    Adding a resource loader, to make testing easier

---
+++ kouchat-android-impl/src/main/java/net/usikkert/kouchat/util/ResourceLoader.java
@@ -0,0 +1,45 @@
+
+/***************************************************************************
+ *   Copyright 2006-2014 by Christian Ihle                                 *
+ *   contact@kouchat.net                                                   *
+ *                                                                         *
+ *   This file is part of KouChat.                                         *
+ *                                                                         *
+ *   KouChat is free software; you can redistribute it and/or modify       *
+ *   it under the terms of the GNU Lesser General Public License as        *
+ *   published by the Free Software Foundation, either version 3 of        *
+ *   the License, or (at your option) any later version.                   *
+ *                                                                         *
+ *   KouChat is distributed in the hope that it will be useful,            *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      *
+ *   Lesser General Public License for more details.                       *
+ *                                                                         *
+ *   You should have received a copy of the GNU Lesser General Public      *
+ *   License along with KouChat.                                           *
+ *   If not, see <http://www.gnu.org/licenses/>.                           *
+ ***************************************************************************/
+
+package net.usikkert.kouchat.util;
+
+import java.net.URL;
+
+/**
+ * Loads resources from the classpath.
+ *
+ * @author Christian Ihle
+ */
+public class ResourceLoader {
+
+    /**
+     * Gets the resource at the specified path. Returns <code>null</code> if the resource doesn't exists.
+     *
+     * @param path Full path to the resource, on the classpath.
+     * @return The {@link URL} to the resource if found, or <code>null</code> if not found.
+     */
+    public URL getResource(final String path) {
+        Validate.notEmpty(path, "Path can not be empty");
+
+        return getClass().getResource(path);
+    }
+}
---
+++ kouchat-android-impl/src/test/java/net/usikkert/kouchat/util/ResourceLoaderTest.java
@@ -0,0 +1,75 @@
+
+/***************************************************************************
+ *   Copyright 2006-2014 by Christian Ihle                                 *
+ *   contact@kouchat.net                                                   *
+ *                                                                         *
+ *   This file is part of KouChat.                                         *
+ *                                                                         *
+ *   KouChat is free software; you can redistribute it and/or modify       *
+ *   it under the terms of the GNU Lesser General Public License as        *
+ *   published by the Free Software Foundation, either version 3 of        *
+ *   the License, or (at your option) any later version.                   *
+ *                                                                         *
+ *   KouChat is distributed in the hope that it will be useful,            *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      *
+ *   Lesser General Public License for more details.                       *
+ *                                                                         *
+ *   You should have received a copy of the GNU Lesser General Public      *
+ *   License along with KouChat.                                           *
+ *   If not, see <http://www.gnu.org/licenses/>.                           *
+ ***************************************************************************/
+
+package net.usikkert.kouchat.util;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+/**
+ * Test of {@link ResourceLoader}.
+ *
+ * @author Christian Ihle
+ */
+@SuppressWarnings("HardCodedStringLiteral")
+public class ResourceLoaderTest {
+
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
+
+    private ResourceLoader resourceLoader;
+
+    @Before
+    public void setUp() {
+        resourceLoader = new ResourceLoader();
+    }
+
+    @Test
+    public void getResourceShouldThrowExceptionIfPathIsNull() {
+        expectedException.expect(IllegalArgumentException.class);
+        expectedException.expectMessage("Path can not be empty");
+
+        resourceLoader.getResource(null);
+    }
+
+    @Test
+    public void getResourceShouldThrowExceptionIfPathIsBlank() {
+        expectedException.expect(IllegalArgumentException.class);
+        expectedException.expectMessage("Path can not be empty");
+
+        resourceLoader.getResource(" ");
+    }
+
+    @Test
+    public void getResourceShouldReturnValidUrlIfFileExists() {
+        assertNotNull(resourceLoader.getResource("/sounds/error.wav"));
+    }
+
+    @Test
+    public void getResourceShouldReturnNullIfFileDoesNotExist() {
+        assertNull(resourceLoader.getResource("/sounds/unknown.wav"));
+    }
+}
